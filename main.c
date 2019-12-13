7819a182e93f74a0d913a944e0a0f3725920335a
IgorZhurbenko <Igor_Zhurbenko@mail.ru>
2019-12-13 21:22:20 +0400

    //Something done

--- main.c
+++ main.c
@@ -1,1 +1,18 @@
+#include <stdio.h>
 
+void readfile(char[] FileName, char *ar)
+{
+	FILE *f = fopen(FileName,"r");
+	fscanf(f,"%d%d", ar[1], ar[2]);	
+	fclose(f);
+}
+
+int main()
+{
+	int ar[2];
+	
+	readfile("in.txt",ar);
+
+	printf("%d%d", ar[1],ar[2]);
+	
+}
